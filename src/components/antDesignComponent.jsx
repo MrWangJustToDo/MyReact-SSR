df68d423a7e07345472fdb1a6077d4a05fe227d2
MrWangJustToDo <2711470541@qq.com>
2022-06-02 23:21:45 +0800

    more fix

--- src/components/antDesignComponent.jsx
+++ src/components/antDesignComponent.jsx
@@ -1,11 +1,49 @@
-import React from "react";
-import { Card } from "antd";
+import React, { useState } from "react";
+import { Calendar, Card, Carousel, Slider, Switch } from "antd";
+import { useEffect } from "react";
+import { useRef } from "react";
+
+const contentStyle = {
+  height: "160px",
+  color: "#fff",
+  lineHeight: "160px",
+  textAlign: "center",
+  background: "#364d79",
+};
 
 const AntDesignComponent = () => {
+  const ref = useRef();
+  const [disabled, setDisabled] = useState(false);
+
+  console.log(ref)
+
+  const onChange = (checked) => {
+    console.log("run", checked);
+    setDisabled(checked);
+  };
+
   return (
     <>
       <h2>Ant Design</h2>
       <Card>123</Card>
+      <Carousel>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+      {/* <Slider defaultValue={30} /> */}
+      {/* <Calendar /> */}
+      <Slider range defaultValue={[20, 50]} disabled={disabled} />
+      Disabled: <Switch ref={ref} size="small" checked={disabled} onChange={onChange} />
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 export default function Home() {
-  return <div>home page</div>;
+  const [state, setState] = useState(new Date().toString());
+  useEffect(() => {
+    const id = setInterval(() => setState(new Date().toString()), 1000);
+    return () => clearInterval(id);
+  }, []);
+  // Math.random() > 0.5 ? useEffect(() => {}) : useState();
+  return <div>home page {state}</div>;
 }
